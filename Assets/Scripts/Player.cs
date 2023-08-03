b9363335e1daae425e97d1179339a245ba2fcf98
rhutmann <pappy.rhutmannjnrolege@gmail.com>
2023-08-03 14:40:15 +0000

    player movement bug fixes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -121,7 +121,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
 
             // attempt only x movement
             Vector3 moveDirX = new Vector3(moveDir.x, 0f, 0f).normalized;
-            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            canMove = moveDir.x != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
 
             if (canMove)
             {
@@ -132,7 +132,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
             {
                 // attempt only z movement
                 Vector3 moveDirZ = new Vector3(0f, 0f, moveDir.z).normalized;
-                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                canMove = moveDir.z != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
 
                 if (canMove)
                 {
