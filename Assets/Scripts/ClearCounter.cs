07efb8396e83516e1907c39c10135f12f4c8a50f
rhutmann <pappy.rhutmannjnrolege@gmail.com>
2023-08-03 14:01:51 +0000

    added ability to take and drop kitchen objects

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -12,8 +12,36 @@ public class ClearCounter : BaseCounter
     public override void Interact(Player player)
     {
 
+        if (!HasKitchenObject())
+        {
+            // theres no kitchenObject here
 
+            if (player.HasKitchenObject())
+            {
+                // player is carrying a kitchen object
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+                // player is not carrying a kitchen object
 
+            }
+
+        }
+        else
+        {
+            // there is a kitchen object
+            if (player.HasKitchenObject())
+            {
+                // player is carrying something
+            }
+            else
+            {
+                // player is not carrying anything
+                GetKitchenObject().SetKitchenObjectParent(player);
+
+            }
+        }
     }
 
 
